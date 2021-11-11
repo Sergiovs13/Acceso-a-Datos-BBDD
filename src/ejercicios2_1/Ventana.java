1c122e60c300b9cfb8623b13cf85d95f53e9b2f2
Sergiovs13 <sergio.vs13@hotmail.com>
2021-11-11 18:02:53 +0100

    Componentes ejercicio2.3 creados

--- src/ejercicios2_1/Ventana.java
+++ src/ejercicios2_1/Ventana.java
@@ -18,6 +18,14 @@ public class Ventana extends JFrame{
 	private JTextField tfDia,tfMes,tfAnyo,tfMatricula;
 	private GestionBBDD gest=new GestionBBDD();
 	private JTextField tfDNIDelete;
+	private JTextField tfDNIM;
+	private JTextField tfNombreM;
+	private JTextField tfApellidoM;
+	private JTextField tfSueldoM;
+	private JTextField tfDiaM;
+	private JTextField tfMesM;
+	private JTextField tfAnyoM;
+	private JTextField tfMatriculaM;
 	
 	public Ventana() {
 		iniciarComponentes();
@@ -27,7 +35,7 @@ public class Ventana extends JFrame{
 	private void iniciarComponentes() {
 		setTitle("Insertar Trabajadores");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
-		setSize(590, 720);
+		setSize(620, 720);
 		setLocationRelativeTo(null);
 		getContentPane().setLayout(null);
 		
@@ -43,11 +51,11 @@ public class Ventana extends JFrame{
 		getContentPane().add(tp);
 		
 		JScrollPane scrollPane = new JScrollPane(tp);
-		scrollPane.setBounds(25, 11, 523, 187);
+		scrollPane.setBounds(25, 11, 554, 187);
 		getContentPane().add(scrollPane);
 		
 		JButton btnVerTrabajadores = new JButton("Ver Trabajadores");
-		btnVerTrabajadores.setBounds(157, 209, 136, 23);
+		btnVerTrabajadores.setBounds(206, 209, 136, 23);
 		getContentPane().add(btnVerTrabajadores);
 		
 		btnVerTrabajadores.addActionListener(new ActionListener() {
@@ -68,7 +76,7 @@ public class Ventana extends JFrame{
 		getContentPane().add(lblDNI);
 		
 		tfDNI = new JTextField();
-		tfDNI.setBounds(123, 298, 136, 20);
+		tfDNI.setBounds(344, 298, 136, 20);
 		getContentPane().add(tfDNI);
 		tfDNI.setColumns(10);
 		
@@ -133,7 +141,7 @@ public class Ventana extends JFrame{
 		tfMatricula.setColumns(10);
 		
 		JButton btnDarAlta = new JButton("Dar Alta");
-		btnDarAlta.setBounds(204, 546, 89, 23);
+		btnDarAlta.setBounds(123, 543, 89, 23);
 		getContentPane().add(btnDarAlta);
 		
 		btnDarAlta.addActionListener(new ActionListener() {
@@ -172,6 +180,89 @@ public class Ventana extends JFrame{
 		JButton btnEliminar = new JButton("Eliminar");
 		btnEliminar.setBounds(265, 647, 89, 23);
 		getContentPane().add(btnEliminar);
+		
+		JLabel lblModificar = new JLabel("Modificar Trabajador");
+		lblModificar.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblModificar.setBounds(327, 252, 136, 14);
+		getContentPane().add(lblModificar);
+		
+		JLabel lblDNIM = new JLabel("DNI:");
+		lblDNIM.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblDNIM.setBounds(308, 301, 46, 14);
+		getContentPane().add(lblDNIM);
+		
+		tfDNIM = new JTextField();
+		tfDNIM.setBounds(123, 297, 136, 20);
+		getContentPane().add(tfDNIM);
+		tfDNIM.setColumns(10);
+		
+		JButton btnBuscar = new JButton("Buscar");
+		btnBuscar.setBounds(490, 297, 89, 23);
+		getContentPane().add(btnBuscar);
+		
+		JLabel lblNombreM = new JLabel("Nombre:");
+		lblNombreM.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblNombreM.setBounds(308, 345, 57, 14);
+		getContentPane().add(lblNombreM);
+		
+		tfNombreM = new JTextField();
+		tfNombreM.setBounds(375, 342, 146, 20);
+		getContentPane().add(tfNombreM);
+		tfNombreM.setColumns(10);
+		
+		JLabel lblApellidoM = new JLabel("Apellidos:");
+		lblApellidoM.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblApellidoM.setBounds(308, 385, 57, 14);
+		getContentPane().add(lblApellidoM);
+		
+		tfApellidoM = new JTextField();
+		tfApellidoM.setBounds(375, 382, 146, 20);
+		getContentPane().add(tfApellidoM);
+		tfApellidoM.setColumns(10);
+		
+		JLabel lblSueldoM = new JLabel("Sueldo:");
+		lblSueldoM.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblSueldoM.setBounds(308, 424, 46, 14);
+		getContentPane().add(lblSueldoM);
+		
+		tfSueldoM = new JTextField();
+		tfSueldoM.setBounds(375, 421, 146, 20);
+		getContentPane().add(tfSueldoM);
+		tfSueldoM.setColumns(10);
+		
+		JLabel lblFechaM = new JLabel("Fecha:");
+		lblFechaM.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblFechaM.setBounds(308, 463, 46, 14);
+		getContentPane().add(lblFechaM);
+		
+		tfDiaM = new JTextField();
+		tfDiaM.setBounds(375, 460, 24, 20);
+		getContentPane().add(tfDiaM);
+		tfDiaM.setColumns(10);
+		
+		tfMesM = new JTextField();
+		tfMesM.setBounds(410, 460, 24, 20);
+		getContentPane().add(tfMesM);
+		tfMesM.setColumns(10);
+		
+		tfAnyoM = new JTextField();
+		tfAnyoM.setBounds(444, 460, 86, 20);
+		getContentPane().add(tfAnyoM);
+		tfAnyoM.setColumns(10);
+		
+		JLabel lblMatriculaM = new JLabel("Matricula:");
+		lblMatriculaM.setFont(new Font("Tahoma", Font.BOLD, 11));
+		lblMatriculaM.setBounds(308, 500, 57, 14);
+		getContentPane().add(lblMatriculaM);
+		
+		tfMatriculaM = new JTextField();
+		tfMatriculaM.setBounds(375, 497, 146, 20);
+		getContentPane().add(tfMatriculaM);
+		tfMatriculaM.setColumns(10);
+		
+		JButton btnModificar = new JButton("Efectuar Cambios");
+		btnModificar.setBounds(375, 543, 146, 23);
+		getContentPane().add(btnModificar);
 		
 		btnEliminar.addActionListener(new ActionListener() {
 
